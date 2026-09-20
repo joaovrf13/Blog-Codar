0cc909d13e8d1d21b9034cefe54945ec1d8263ab
JoaoVrf13 <joaovictorrfcontato@gmail.com>
2026-09-20 19:22:16 -0300

    Commit inicial

---
+++ contato.php
@@ -0,0 +1,8 @@
+<?php
+include_once("templates/header.php");
+?>
+<body>
+    <h1>Pagina de contato</h1>
+<?php
+include_once("templates/header.php");
+?>
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>&copy; 2026 Blog Codar. Todos os direitos reservados.</p>
+</footer>
+
+</body>
+</html>
