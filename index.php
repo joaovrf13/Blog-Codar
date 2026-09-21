4b69ccea377357f8cd279d98ad39dc7e58e1681c
JoaoVrf13 <joaovictorrfcontato@gmail.com>
2026-09-21 20:28:39 -0300

    feat: finaliza layout da página inicial

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
  include_once("templates/header.php");
 ?>
-    <h1>Olá mundo!</h1>
+   <main id="title-container">
+     <div>
+       <h1>Blog Codar</h1>
+       <p>O seu blog de programação</p>
+     </div>
+     <div id="posts-container">
+       <?php foreach($posts as $post): ?>
+         <div class="post-box">
+             <img src="<?= $BASE_URL?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+             <h2 class="post-title">
+                <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>" class="btn btn-primary">Ler mais</a>
+            </h2>
+            <p class="post-description"><?= $post['description'] ?></p>     
+            <div class="tags-container">
+                <?php foreach($post['tags'] as $tag): ?>
+                    <a href="#"><?= $tag ?></a>
+                <?php endforeach; ?>  
+         </div>   
+       <?php endforeach; ?>
+     </div>
+   </main>
 <?php
  include_once("templates/footer.php");
 ?>
